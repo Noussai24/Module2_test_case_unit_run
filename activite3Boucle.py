7ad35834d47d60dc9b08beec86bb0691047b3880
unknown <imenhzl14@gmail.com>
2024-04-23 10:43:31 +0200

    👌 IMPROVE: test Runner activite Boucle validé

--- activite3Boucle.py
+++ activite3Boucle.py
@@ -3,26 +3,31 @@ from unittest.mock import patch
 
 
 def nombreGrand():
-    nombres = [] # j'initialise une liste vide pour enregistrer les nombres
-    for i in range(3): # on veut juste trois nombre
-        nombre = int(input("Entrez le nombre : ")) 
+    # j'initialise une liste vide pour enregistrer les nombres
+    nombres = []
+    # on veut juste trois nombre
+    for i in range(3):
+        nombre = int(input("Entrez le nombre : "))
         nombres.append(nombre)
     plus_grand_nombre = nombres[0]
     for elt in nombres:
         if elt > plus_grand_nombre:
             plus_grand_nombre = elt
     return plus_grand_nombre
+# Test Unitaire: TestRunner
+
 
 class TestNombreGrandFunction(unittest.TestCase):
     @patch('builtins.input', side_effect=[1, 7, 9])
-    def test_nombre(self, mock_input):
+    def test_nombre_positif(self, mock_input):
         self.assertEqual(nombreGrand(),9)
     @patch('builtins.input', side_effect=[-3, -6, -7])
     def test_nombre_negatif(self, mock_input):
         self.assertEqual(nombreGrand(),-3)
     @patch('builtins.input', side_effect=[-3, 0, 6])
     def test_nombre_melange(self, mock_input):
-        self.assertEqual(nombreGrand(),6)
+        self.assertEqual(nombreGrand(), 6)
+
 
 class MyTestRunner(unittest.TextTestRunner):
     def run(self, test):
@@ -38,5 +43,3 @@ if __name__ == '__main__':
     suite.addTest(TestNombreGrandFunction('test_nombre_melange'))
     runner = MyTestRunner(verbosity=2)
     runner.run(suite)
-
-
